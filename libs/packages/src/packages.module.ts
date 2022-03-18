e6344241ddc6c1460d98c1d5b38ae882d5da25a9
Razin Idzuddin <razin99@gmail.com>
2022-03-18 17:34:42 +1100

    chore: remove unused libraries

--- libs/packages/src/packages.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PackagesService } from './packages.service';
-
-@Module({
-  providers: [PackagesService],
-  exports: [PackagesService],
-})
-export class PackagesModule {}
--- libs/projects/src/projects.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
-
-@Module({
-  providers: [ProjectsService],
-  exports: [ProjectsService],
-})
-export class ProjectsModule {}
