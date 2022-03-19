89d4743a852573f77f4bac48cb1da8f18e650686
Razin Idzuddin <razin99@gmail.com>
2022-03-20 00:58:23 +1100

    feat(packages): add path to get all vulns in a package

--- apps/http/src/packages/packages.controller.ts
+++ apps/http/src/packages/packages.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { QueryController } from '../query-service.abstract';
 import { PackagesService } from './packages.service';
 
@@ -6,5 +12,14 @@ import { PackagesService } from './packages.service';
 export class PackagesController extends QueryController {
   constructor(private readonly packagesService: PackagesService) {
     super(packagesService);
+  }
+
+  // TODO: handle pagination of vulns?
+  @Get(':id/vulns')
+  async vulnsInPackage(@Param('id', new ParseUUIDPipe()) id: string) {
+    const response = await this.packagesService.findOne(id);
+    if (!response) throw new NotFoundException();
+    await response.populate();
+    return response;
   }
 }
