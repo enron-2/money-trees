2cccf29a1665b18950bee3b3e28c1b070e534087
Razin Idzuddin <razin99@gmail.com>
2022-03-23 16:33:06 +1100

    feat(config): add schema validator for env variables

--- apps/http/src/http.module.ts
+++ apps/http/src/http.module.ts
@@ -1,5 +1,7 @@
 import { Global, Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SchemaModule } from '@schemas/module';
+import * as Joi from 'joi';
 import { DynamooseModule } from 'nestjs-dynamoose';
 import { PackagesModule } from './packages/packages.module';
 import { ProjectsModule } from './projects/projects.module';
@@ -13,6 +15,15 @@ export class GlobalSchema {}
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DYNA_PORT: Joi.number().min(0).default(8000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
+    }),
     process.env.NODE_ENV === 'production'
       ? DynamooseModule.forRoot({ aws: { region: process.env.REGION }, logger })
       : DynamooseModule.forRoot({
