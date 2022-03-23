7b272d6e7de7cf60f774b404ca07ad5c2ec0a4ba
Razin Idzuddin <razin99@gmail.com>
2022-03-23 16:16:26 +1100

    feat(http): include class serializer globally

--- apps/http/src/main.ts
+++ apps/http/src/main.ts
@@ -1,5 +1,5 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpModule } from './http.module';
 
@@ -9,11 +9,13 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Money Tr33s')
     .setDescription('The perfect place for shade')
+    .setVersion('Untested, response schema may be incorrect')
     .build();
   const docs = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, docs);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(3000);
 }
