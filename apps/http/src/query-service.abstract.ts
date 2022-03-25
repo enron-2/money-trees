056a74b066d561904f984c75190d54606b37f46c
Razin Idzuddin <razin99@gmail.com>
2022-03-25 22:13:58 +1100

    feat(query-service): extend functionality to allow search on any attribute on model

--- apps/http/src/query-service.abstract.ts
+++ apps/http/src/query-service.abstract.ts
@@ -8,21 +8,28 @@ export abstract class QueryService<Data, Key> extends TransactionSupport {
     super();
   }
 
-  findAll(limit = 10, lastKey?: string) {
-    let query = this.repository.scan().limit(limit);
-    if (lastKey) {
-      query = query.startAt({ id: lastKey });
+  async findAll(limit = 10, lastKey?: string, query?: Record<string, any>) {
+    let scanner = this.repository.scan();
+    for (const [k, v] of Object.entries(query)) {
+      if (!v) continue;
+      scanner =
+        typeof v === 'string'
+          ? scanner.and().where(k).contains(v)
+          : scanner.and().where(k).eq(v);
     }
-    return query.exec();
+    if (lastKey) {
+      scanner = scanner.startAt({ id: lastKey });
+    }
+    if (Object.values(query).filter((v) => !!v).length > 0) {
+      const res = await scanner.exec();
+      return res.slice(0, limit);
+    } else {
+      return scanner.limit(limit).exec();
+    }
   }
 
   async findOne(id: string) {
-    const [res] = await this.repository
-      .query()
-      .where('id')
-      .eq(id)
-      .limit(1)
-      .exec();
+    const [res] = await this.repository.query().where('id').eq(id).exec();
     return res;
   }
 }
