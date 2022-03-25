2be2c58150e1190b05955633fee92780099b557c
Razin Idzuddin <razin99@gmail.com>
2022-03-25 15:29:21 +1100

    feat(vulns): add add and remove vuln route
    still needs to add transaction to create and remove vuln for atomicity

--- apps/http/src/vulns/vulns.service.ts
+++ apps/http/src/vulns/vulns.service.ts
@@ -23,10 +23,10 @@ export class VulnsService extends QueryService<
     const pkg = await this.pkgSvc.findOne(packageId);
     if (!pkg.vulns && !Array.isArray(pkg.vulns)) {
       // First vuln on package
-      pkg.vulns = [packageId] as any;
+      pkg.vulns = [vulnId] as any;
     } else if (!pkg.vulns.find((v: any) => v === vulnId)) {
       // Add new vuln to package
-      pkg.vulns.push(packageId as any);
+      pkg.vulns.push(vulnId as any);
     } else {
       // vuln found in package, no updates
       return;
@@ -34,32 +34,33 @@ export class VulnsService extends QueryService<
     return pkg;
   }
 
-  async unlinkFromPackage(vulnId: string, packageId: string) {
-    const pkg = await this.pkgSvc.findOne(packageId);
-    if (!pkg.vulns && !Array.isArray(pkg.vulns)) {
-      // no vulns in package
-      return;
-    } else if (!pkg.vulns.find((v: any) => v === vulnId)) {
-      // specific vuln not in package
-      return;
-    }
-    pkg.vulns = pkg.vulns.filter((v: any) => v !== vulnId);
-    return pkg;
+  async unlinkFromPackage(vulnId: string) {
+    const pkgs = await this.pkgSvc.packages
+      .scan()
+      .where('vulns')
+      .contains(vulnId)
+      .all()
+      .exec();
+    return pkgs.map((pkg) => ({
+      ...pkg,
+      vulns: pkg.vulns.filter((vId: any) => vId !== vulnId),
+    }));
   }
 
   async create(input: CreateVulnInput) {
     const { packageIds, ...data } = input;
     const vulnId = uuid();
     const pkgs = await Promise.all(
-      packageIds.map((id) => this.linkToPackage(vulnId, id)).filter((p) => !!p),
+      packageIds.map((id) => this.linkToPackage(vulnId, id)),
+    ).then((res) => res.filter((p) => !!p));
+
+    // TODO: wrap this section in Transaction
+    const newVuln = await this.vulns.create({ id: vulnId, ...data });
+    await Promise.all(
+      pkgs.map(({ id, ...data }) => this.pkgSvc.packages.update({ id }, data)),
     );
 
-    await this.transaction([
-      this.vulns.transaction.create(data),
-      ...pkgs.map((p) => this.pkgSvc.packages.transaction.update(p)),
-    ]);
-
-    return this.vulns.get({ id: vulnId });
+    return newVuln;
   }
 
   async update(id: string, input: UpdateVulnInput) {
@@ -68,12 +69,16 @@ export class VulnsService extends QueryService<
     return this.vulns.update({ id }, input);
   }
 
-  // TODO: 'unlink' or 'cascade delete' on Package model
   async delete(id: string) {
     const toDelete = await this.vulns.get({ id });
     if (!toDelete) throw new NotFoundException();
+    const pkgs = await this.unlinkFromPackage(id);
 
-    await this.transaction([this.vulns.transaction.delete({ id })]);
+    // TODO: wrap this section in Transaction
+    await Promise.all(
+      pkgs.map(({ id, ...data }) => this.pkgSvc.packages.update({ id }, data)),
+    );
+    await this.vulns.delete({ id });
 
     return toDelete;
   }
