7205ada13f879972196513cd2c4213fc74d7b812
Razin Idzuddin <razin99@gmail.com>
2022-04-01 16:07:40 +1100

    feat(deployment): create different 'apps' for staging and prod environment

--- apps/deployment/src/main.ts
+++ apps/deployment/src/main.ts
@@ -1,16 +1,29 @@
 import { App, Environment } from '@aws-cdk/core';
 import { DatabaseStack, HttpStack, ParserStack } from './stacks';
 
-const app = new App();
 const env: Environment = {
   region: 'ap-southeast-2',
 };
-const database = new DatabaseStack(app, 'Database', { env });
-new HttpStack(app, 'HttpStack', {
+const app = new App();
+
+const stagingDb = new DatabaseStack(app, 'StagingDatabase', { env });
+new HttpStack(app, 'StagingHttpStack', {
   env,
-  database,
+  database: stagingDb,
+  stageName: 'staging',
 });
-new ParserStack(app, 'ParserStack', {
+new ParserStack(app, 'StagingParserStack', {
   env,
-  database,
+  database: stagingDb,
+});
+
+const prodDb = new DatabaseStack(app, 'ProdDatabase', { env });
+new HttpStack(app, 'ProdHttpStack', {
+  env,
+  database: prodDb,
+  stageName: 'prod',
+});
+new ParserStack(app, 'ProdParserStack', {
+  env,
+  database: prodDb,
 });
