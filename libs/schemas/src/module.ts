3a439a3884a31f3f3c468bf49a8ca4bd62eaa05a
Razin Idzuddin <razin99@gmail.com>
2022-04-01 15:00:21 +1100

    feat(dynamoose): extend forFeatureAsync to allow custom provider token
    Needed since table name is not necessarily the same as provider token when loading table name from
    environment

--- libs/schemas/src/module.ts
+++ libs/schemas/src/module.ts
@@ -1,29 +1,72 @@
-import { Module } from '@nestjs/common';
-import { DynamooseModule } from 'nestjs-dynamoose';
+import { DynamicModule, flatten, Module } from '@nestjs/common';
+import { DynamooseModule, getModelToken } from 'nestjs-dynamoose';
+import { AsyncModelFactory } from 'nestjs-dynamoose/dist/interfaces/async-model-factory.interface';
+import { DYNAMOOSE_INITIALIZATION } from 'nestjs-dynamoose/dist/dynamoose.constants';
+import * as dynamoose from 'dynamoose';
 import { createPackageSchema } from './packages';
 import { createProjectSchema } from './projects';
 import { createVulnerabilitySchema } from './vulnerabilities';
 import { Package, Project, Vuln } from './tablenames';
 
-const dynamoose = DynamooseModule.forFeatureAsync([
+interface CustomAsyncModelFactory extends AsyncModelFactory {
+  provide?: string;
+}
+class CustomDynamooseModule extends DynamooseModule {
+  static forFeatureAsync(factories?: CustomAsyncModelFactory[]): DynamicModule {
+    const providers = (factories || []).map((model) => [
+      {
+        provide: getModelToken(model.provide ?? model.name),
+        useFactory: async (...args: unknown[]) => {
+          const schema = await model.useFactory(...args);
+          const modelInstance = dynamoose.model(
+            model.name,
+            schema,
+            model.options
+          );
+          if (model.serializers) {
+            Object.entries(model.serializers).forEach(([key, value]) => {
+              modelInstance.serializer.add(key, value);
+            });
+          }
+          return modelInstance;
+        },
+        inject: [DYNAMOOSE_INITIALIZATION, ...(model.inject || [])],
+      },
+    ]);
+    const imports = factories.map((factory) => factory.imports || []);
+    const uniqImports = new Set(flatten(imports));
+    const flatProviders = flatten(providers);
+    return {
+      module: DynamooseModule,
+      imports: [...uniqImports],
+      providers: flatProviders,
+      exports: flatProviders,
+    };
+  }
+}
+
+const customDynamooseModule = CustomDynamooseModule.forFeatureAsync([
   {
-    name: Vuln,
+    name: process.env.VULN_TABLE ?? Vuln,
+    provide: Vuln,
     useFactory: () => createVulnerabilitySchema(),
   },
   {
-    name: Package,
+    name: process.env.PACKAGE_TABLE ?? Package,
+    provide: Package,
     inject: ['VulnModel'],
     useFactory: (_, model) => createPackageSchema(model),
   },
   {
-    name: Project,
+    name: process.env.PROJECT_TABLE ?? Project,
+    provide: Project,
     inject: ['PackageModel'],
     useFactory: (_, model) => createProjectSchema(model),
   },
 ]);
 
 @Module({
-  imports: [dynamoose],
-  exports: [dynamoose],
+  imports: [customDynamooseModule],
+  exports: [customDynamooseModule],
 })
 export class SchemaModule {}
