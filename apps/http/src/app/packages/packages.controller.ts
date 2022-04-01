d8e0650a649d957d57b290998f8d3c770ebea053
Razin Idzuddin <razin99@gmail.com>
2022-04-01 19:59:21 +1100

    fix(http): paginate relational data fields
    accepts same sets of input as normal pagination (limit, lastKey), manually slice array before
    calling populate and returning the data.

--- apps/http/src/app/packages/packages.controller.ts
+++ apps/http/src/app/packages/packages.controller.ts
@@ -24,7 +24,7 @@ import { PaginationDto } from '../query-service.abstract';
 import { PackagesService } from './packages.service';
 
 class PackageSearchInputDto extends PartialType(
-  IntersectionType(OmitType(PackageDto, ['id', 'createdAt']), PaginationDto),
+  IntersectionType(OmitType(PackageDto, ['id', 'createdAt']), PaginationDto)
 ) {
   @ApiPropertyOptional({ type: Number })
   @IsOptional()
@@ -50,7 +50,7 @@ export class PackagesController {
   @Get()
   findAll(
     @Query()
-    { limit, lastKey, ...query }: PackageSearchInputDto,
+    { limit, lastKey, ...query }: PackageSearchInputDto
   ): Promise<PackageDto[]> {
     return this.packagesService.findAll(limit, lastKey, query);
   }
@@ -62,14 +62,13 @@ export class PackagesController {
   @UseInterceptors(new DtoConformInterceptor(PackageDto))
   @Get(':id')
   async findOne(
-    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('id', new ParseUUIDPipe()) id: string
   ): Promise<PackageDto> {
     const res = await this.packagesService.findOne(id);
     if (!res) throw new NotFoundException();
     return res;
   }
 
-  // TODO: handle pagination of vulns?
   @ApiOperation({
     summary: 'Package with given ID including vulnerabilities',
   })
@@ -80,9 +79,24 @@ export class PackagesController {
   @Get(':id/vulns')
   async vulnsInPackage(
     @Param('id', new ParseUUIDPipe()) id: string,
+    @Query() { lastKey, limit = 10 }: PaginationDto
   ): Promise<PackageDetailDto> {
     const response = await this.packagesService.findOne(id);
     if (!response) throw new NotFoundException();
+
+    const vulnIds = response.vulns as unknown as string[];
+    let lastKeyIdx: number;
+    if (lastKey) {
+      lastKeyIdx = vulnIds.indexOf(lastKey);
+      if (lastKeyIdx < 0) throw new NotFoundException('lastKey not found');
+      response.vulns =
+        response.vulns?.length - 1 > lastKeyIdx
+          ? response.vulns.slice(lastKeyIdx + 1, lastKeyIdx + limit)
+          : []; // no more items after lastKey
+    } else {
+      response.vulns = response.vulns?.slice(0, limit);
+    }
+
     await response.populate();
     return response;
   }
