88b4f31dc6dc7557e55214f1acc3187794513fd5
Razin Idzuddin <razin99@gmail.com>
2022-04-01 12:18:39 +1100

    refactor(deployment): create lambda construct to define default NODE lambda settings

---
+++ apps/deployment/src/constructs/index.ts
@@ -0,0 +1,38 @@
+import { Construct, Duration } from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+
+export interface NodeLambdaFuncProps
+  extends Omit<lambda.FunctionProps, 'runtime' | 'handler'> {
+  code: lambda.Code;
+}
+
+const generateEnvs = (env?: Record<string, string>) => ({
+  NODE_ENV: 'production',
+  NO_COLOR: 'true',
+  ...env,
+});
+
+/**
+ * Create `NODEJS_14_X` lambda function with defaults
+ *
+ * runtime: `NODEJS_14_X`
+ * handler: 'main.handler'
+ * memorySize: 256MB
+ * timeout: 5 seconds
+ * environment: `{ NODE_ENV: 'production', NO_COLOR: 'true' }`
+ *
+ */
+export class NodeLambdaFunc extends Construct {
+  LambdaFunction: lambda.Function;
+  constructor(scope: Construct, id: string, props: NodeLambdaFuncProps) {
+    super(scope, id);
+    this.LambdaFunction = new lambda.Function(this, id, {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'main.handler',
+      memorySize: 256,
+      timeout: Duration.seconds(5),
+      environment: generateEnvs(props.environment),
+      ...props,
+    });
+  }
+}
--- apps/deployment/src/main.ts
+++ apps/deployment/src/main.ts
@@ -1,28 +1,16 @@
-import { App, Duration, Environment } from '@aws-cdk/core';
-import type { FunctionProps } from '@aws-cdk/aws-lambda';
+import { App, Environment } from '@aws-cdk/core';
 import { DatabaseStack, HttpStack, ParserStack } from './stacks';
 
 const app = new App();
 const env: Environment = {
   region: 'ap-southeast-2',
 };
-const lambdaConfig: Partial<FunctionProps> = {
-  memorySize: 256,
-  timeout: Duration.seconds(5),
-  environment: {
-    NODE_ENV: 'production',
-    NO_COLOR: 'true',
-  },
-};
-
 const database = new DatabaseStack(app, 'Database', { env });
 new HttpStack(app, 'HttpStack', {
   env,
   database,
-  lambdaConfig,
 });
 new ParserStack(app, 'ParserStack', {
   env,
   database,
-  lambdaConfig,
 });
