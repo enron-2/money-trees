427fec4306c0c2d38c1195c31c25828965d8cdb6
Razin Idzuddin <razin99@gmail.com>
2022-04-01 15:06:07 +1100

    refactor(dynamoose): moved custom dynamoose module to core libs

--- libs/schemas/src/module.ts
+++ libs/schemas/src/module.ts
@@ -1,49 +1,9 @@
-import { DynamicModule, flatten, Module } from '@nestjs/common';
-import { DynamooseModule, getModelToken } from 'nestjs-dynamoose';
-import { AsyncModelFactory } from 'nestjs-dynamoose/dist/interfaces/async-model-factory.interface';
-import { DYNAMOOSE_INITIALIZATION } from 'nestjs-dynamoose/dist/dynamoose.constants';
-import * as dynamoose from 'dynamoose';
+import { Module } from '@nestjs/common';
+import { CustomDynamooseModule } from '@core/customDynamoose';
 import { createPackageSchema } from './packages';
 import { createProjectSchema } from './projects';
 import { createVulnerabilitySchema } from './vulnerabilities';
 import { Package, Project, Vuln } from './tablenames';
-
-interface CustomAsyncModelFactory extends AsyncModelFactory {
-  provide?: string;
-}
-class CustomDynamooseModule extends DynamooseModule {
-  static forFeatureAsync(factories?: CustomAsyncModelFactory[]): DynamicModule {
-    const providers = (factories || []).map((model) => [
-      {
-        provide: getModelToken(model.provide ?? model.name),
-        useFactory: async (...args: unknown[]) => {
-          const schema = await model.useFactory(...args);
-          const modelInstance = dynamoose.model(
-            model.name,
-            schema,
-            model.options
-          );
-          if (model.serializers) {
-            Object.entries(model.serializers).forEach(([key, value]) => {
-              modelInstance.serializer.add(key, value);
-            });
-          }
-          return modelInstance;
-        },
-        inject: [DYNAMOOSE_INITIALIZATION, ...(model.inject || [])],
-      },
-    ]);
-    const imports = factories.map((factory) => factory.imports || []);
-    const uniqImports = new Set(flatten(imports));
-    const flatProviders = flatten(providers);
-    return {
-      module: DynamooseModule,
-      imports: [...uniqImports],
-      providers: flatProviders,
-      exports: flatProviders,
-    };
-  }
-}
 
 const customDynamooseModule = CustomDynamooseModule.forFeatureAsync([
   {
@@ -54,13 +14,13 @@ const customDynamooseModule = CustomDynamooseModule.forFeatureAsync([
   {
     name: process.env.PACKAGE_TABLE ?? Package,
     provide: Package,
-    inject: ['VulnModel'],
+    inject: [`${Vuln}Model`],
     useFactory: (_, model) => createPackageSchema(model),
   },
   {
     name: process.env.PROJECT_TABLE ?? Project,
     provide: Project,
-    inject: ['PackageModel'],
+    inject: [`${Package}Model`],
     useFactory: (_, model) => createProjectSchema(model),
   },
 ]);
