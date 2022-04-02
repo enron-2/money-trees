f2ee36a23ec10e3990f358a894068cf70e9b00a4
Razin Idzuddin <razin99@gmail.com>
2022-04-02 18:41:56 +1100

    feat: export projectSvc to be used by package module

--- apps/http/src/app/packages/packages.module.ts
+++ apps/http/src/app/packages/packages.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PackagesService } from './packages.service';
 import { PackagesController } from './packages.controller';
+import { ProjectsModule } from '../projects/projects.module';
 
 @Module({
-  controllers: [PackagesController],
+  imports: [ProjectsModule],
   providers: [PackagesService],
   exports: [PackagesService],
+  controllers: [PackagesController],
 })
 export class PackagesModule {}
