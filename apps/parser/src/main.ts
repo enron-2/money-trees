9e7f02238726359d33a1088a649cc72782f8a27f
Razin Idzuddin <razin99@gmail.com>
2022-04-07 15:10:09 +1000

    feat(parser): lambda handler uses new api to consume lock files

--- apps/parser/src/main.ts
+++ apps/parser/src/main.ts
@@ -10,28 +10,29 @@ export const handler: Handler = async (event: S3CreateEvent) => {
   const parser = app.get(ParserService);
   const s3 = new S3({ logger: new Logger('aws-sdk/S3') });
 
-  const results = await Promise.all(
-    event.Records.map(async (record) => {
-      const key = decodeURIComponent(record.s3.object.key);
-      const bucketName = record.s3.bucket.arn.split(':').pop();
-      const file = await s3
-        .getObject({
-          Key: key,
-          Bucket: bucketName,
-        })
-        .promise();
-
-      if (file.ContentLength === 0 || !file.Body)
-        throw new Error('Body cannot be empty');
-
-      const [owner, name] = key.split('/');
-      return await parser.parseFileContents(file.Body.toString(), {
-        owner,
-        name,
-      });
-    })
-  );
-
+  const results: string[] = [];
+  for (const record of event.Records) {
+    const key = decodeURIComponent(record.s3.object.key);
+    const bucketName = record.s3.bucket.arn.split(':').pop();
+    const file = await s3
+      .getObject({
+        Key: key,
+        Bucket: bucketName,
+      })
+      .promise();
+    if (file.ContentLength === 0 || !file.Body)
+      throw new Error('Body cannot be empty');
+    const lockFileContent = await parser.createLockFile(
+      file.Body.toString(),
+      new Logger('createLockFile')
+    );
+    const [owner, name] = key.split('/');
+    const res = await parser.saveFileContents(lockFileContent, {
+      owner,
+      name,
+    });
+    results.push(res);
+  }
   await app.close();
   return results;
 };
