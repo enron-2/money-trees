0eb4cda5f543c39924104eff14fe22ae91ec620a
Razin Idzuddin <razin99@gmail.com>
2022-04-07 20:47:48 +1000

    feat(http): packages controller fully works with new table design

--- apps/http/src/app/packages/packages.controller.ts
+++ apps/http/src/app/packages/packages.controller.ts
@@ -3,35 +3,33 @@ import {
   Get,
   NotFoundException,
   Param,
-  ParseUUIDPipe,
   Query,
   UseInterceptors,
 } from '@nestjs/common';
 import {
   ApiOkResponse,
   ApiOperation,
-  ApiPropertyOptional,
+  ApiQuery,
   ApiTags,
   IntersectionType,
   OmitType,
   PartialType,
 } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsNumber, IsOptional } from 'class-validator';
-import { PackageDetailDto, PackageDto, ProjectDto } from '../dto';
+import { PackageEntity } from '@schemas/entities';
+import { EnumValidationPipe } from '@core/pipes';
+import { SortOrder } from 'dynamoose/dist/General';
+import {
+  PackageDetailDto,
+  PackageDto,
+  PaginationDto,
+  ProjectDto,
+} from '../dto';
 import { DtoConformInterceptor } from '../dto-conform.interceptor';
-import { PaginationDto } from '../query-service.abstract';
 import { PackagesService } from './packages.service';
 
 class PackageSearchInputDto extends PartialType(
-  IntersectionType(OmitType(PackageDto, ['id', 'createdAt']), PaginationDto)
-) {
-  @ApiPropertyOptional({ type: Number })
-  @IsOptional()
-  @Transform((param) => +param.value)
-  @IsNumber()
-  createdAt?: number;
-}
+  IntersectionType(OmitType(PackageEntity, ['type', 'id']), PaginationDto)
+) {}
 
 @ApiTags('Packages')
 @Controller('packages')
@@ -52,7 +50,7 @@ export class PackagesController {
     @Query()
     { limit, lastKey, ...query }: PackageSearchInputDto
   ): Promise<PackageDto[]> {
-    return this.packagesService.findAll(limit, lastKey, query, PackageDto);
+    return this.packagesService.findAll(limit, lastKey, query);
   }
 
   @ApiOperation({ summary: 'Package with given ID' })
@@ -61,12 +59,10 @@ export class PackagesController {
   })
   @UseInterceptors(new DtoConformInterceptor(PackageDto))
   @Get(':id')
-  async findOne(
-    @Param('id', new ParseUUIDPipe()) id: string
-  ): Promise<PackageDto> {
-    const res = await this.packagesService.findOne(id, PackageDto);
-    if (!res) throw new NotFoundException();
-    return res;
+  async findOne(@Param('id') id: string): Promise<PackageDto> {
+    const pkg = await this.packagesService.findOne(id);
+    if (!pkg) throw new NotFoundException();
+    return pkg;
   }
 
   @ApiOperation({
@@ -75,30 +71,21 @@ export class PackagesController {
   @ApiOkResponse({
     type: PackageDetailDto,
   })
+  @ApiQuery({
+    name: 'sort',
+    enum: SortOrder,
+    description: 'Sorts vulnerabilities returned',
+    required: false,
+  })
   @UseInterceptors(new DtoConformInterceptor(PackageDetailDto))
   @Get(':id/vulns')
   async vulnsInPackage(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Query() { lastKey, limit = 10 }: PaginationDto
+    @Param('id') id: string,
+    @Query() { lastKey, limit }: PaginationDto,
+    @Query('sort', new EnumValidationPipe(SortOrder, { optional: true }))
+    sort?: SortOrder
   ): Promise<PackageDetailDto> {
-    const response = await this.packagesService.findOne(id, PackageDetailDto);
-    if (!response) throw new NotFoundException();
-
-    const vulnIds = response.vulns as unknown as string[];
-    let lastKeyIdx: number;
-    if (lastKey) {
-      lastKeyIdx = vulnIds.indexOf(lastKey);
-      if (lastKeyIdx < 0) throw new NotFoundException('lastKey not found');
-      response.vulns =
-        response.vulns?.length - 1 > lastKeyIdx
-          ? response.vulns.slice(lastKeyIdx + 1, lastKeyIdx + limit)
-          : []; // no more items after lastKey
-    } else {
-      response.vulns = response.vulns?.slice(0, limit);
-    }
-
-    await response.populate();
-    return response;
+    return this.packagesService.findOneWithVulns(id, lastKey, sort, limit);
   }
 
   @ApiOperation({
@@ -107,12 +94,25 @@ export class PackagesController {
   @ApiOkResponse({
     type: [ProjectDto],
   })
+  @ApiQuery({
+    name: 'sort',
+    enum: SortOrder,
+    description: 'Package name alphanumeric sorting',
+    required: false,
+  })
   @UseInterceptors(new DtoConformInterceptor(ProjectDto))
   @Get(':id/projects')
   async projectsUsingPackage(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Query() { lastKey, limit = 10 }: PaginationDto
+    @Param('id') id: string,
+    @Query() { lastKey, limit }: PaginationDto,
+    @Query('sort', new EnumValidationPipe(SortOrder, { optional: true }))
+    sort?: SortOrder
   ) {
-    return this.packagesService.findProjectConsumingPackage(id, limit, lastKey);
+    return this.packagesService.findProjectConsumingPackage(
+      id,
+      lastKey,
+      sort,
+      limit
+    );
   }
 }
--- apps/http/src/app/packages/packages.service.ts
+++ apps/http/src/app/packages/packages.service.ts
@@ -1,34 +1,136 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Package, PackageKey } from '@schemas/packages';
+import {
+  PrjDocument,
+  PrjDocumentKey,
+  PkgVulnDocument,
+  PkgVulnDocumentKey,
+} from '@schemas/tables';
+import { AttributeType, normalizeAttributes } from '@core/utils';
+import * as tablenames from '@schemas/tablenames';
 import { InjectModel, Model } from 'nestjs-dynamoose';
-import { ProjectDto } from '../dto';
-import { ProjectsService } from '../projects/projects.service';
-import { QueryService } from '../query-service.abstract';
+import { PackageDetailDto, PackageDto, ProjectDto, VulnDto } from '../dto';
+import { SortOrder } from 'dynamoose/dist/General';
+import { VulnEntity } from '@schemas/entities';
+import { plainToInstance } from 'class-transformer';
+
+type PkgVulnModel = Model<PkgVulnDocument, PkgVulnDocumentKey, 'id' | 'type'>;
+type PrjModel = Model<PrjDocument, PrjDocumentKey, 'id' | 'type'>;
 
 @Injectable()
-export class PackagesService extends QueryService<Package, PackageKey> {
+export class PackagesService {
   constructor(
-    @InjectModel('Package')
-    readonly packages: Model<Package, PackageKey, 'id'>,
-    readonly projectSvc: ProjectsService
+    @InjectModel(tablenames.PackageVuln)
+    private readonly pkgVuln: PkgVulnModel,
+    @InjectModel(tablenames.Project)
+    private readonly prj: PrjModel
+  ) {}
+
+  async findAll(
+    limit = 10,
+    lastKey?: string,
+    query?: Record<string, any>,
+    attrs?: AttributeType<unknown>
   ) {
-    super(packages);
+    const scanner = this.pkgVuln.scan();
+    scanner.where('id').beginsWith('PKG#');
+    scanner.and().where('type').beginsWith('PKG#');
+    if (lastKey) {
+      scanner.startAt({ id: lastKey, type: lastKey });
+    }
+
+    scanner.attributes(
+      attrs ? normalizeAttributes(attrs) : normalizeAttributes(PackageDto)
+    );
+
+    for (const [k, v] of Object.entries(query)) {
+      if (!v) continue;
+      if (typeof v === 'string') scanner.and().where(k).contains(v);
+      else scanner.and().where(k).eq(v);
+    }
+
+    if (Object.values(query).filter((v) => !!v).length > 0) {
+      const res = await scanner.exec();
+      return res.slice(0, limit) as PackageDto[];
+    } else {
+      return scanner.limit(limit).exec() as Promise<PackageDto[]>;
+    }
+  }
+
+  async findOne(id: string, attrs?: AttributeType<unknown>) {
+    return this.pkgVuln.get(
+      {
+        id,
+        type: id,
+      },
+      {
+        return: 'document',
+        attributes: attrs
+          ? normalizeAttributes(attrs)
+          : normalizeAttributes(PackageDto),
+      }
+    ) as Promise<PackageDto>;
+  }
+
+  async findOneWithVulns(
+    id: string,
+    lastKey?: string,
+    sort?: SortOrder,
+    limit = 10
+  ) {
+    const pkg = await this.pkgVuln.get({ id, type: id });
+    if (!pkg) throw new NotFoundException();
+    const queryBuilder = this.pkgVuln
+      .query()
+      .where('severity')
+      .exists()
+      .and()
+      .where('id')
+      .eq(id)
+      .attributes(normalizeAttributes(VulnEntity));
+    if (lastKey) {
+      queryBuilder.startAt({ id, type: lastKey });
+    }
+    if (sort) {
+      queryBuilder.sort(sort);
+    }
+
+    // Skip the 'PKG' entry
+    if (!sort || sort === SortOrder.ascending) {
+      queryBuilder.limit(limit + 1);
+    } else {
+      queryBuilder.limit(limit);
+    }
+
+    const vulns = await queryBuilder.exec();
+    const pkgEntity = plainToInstance(PackageDetailDto, pkg);
+    pkgEntity.vulns = plainToInstance(VulnDto, vulns);
+    return pkgEntity;
   }
 
   async findProjectConsumingPackage(
     packageId: string,
-    limit = 10,
-    lastKey?: string
+    lastKey?: string,
+    sort?: SortOrder,
+    limit = 10
   ) {
-    const { id } = (await super.findOne(packageId, ['id'])) || {};
-    if (!id) throw new NotFoundException();
-    const scanner = this.projectSvc.projects
-      .scan()
-      .where('packages')
-      .contains(packageId)
-      .attributes(this.projectSvc.normalizeAttributes(ProjectDto));
-
-    if (lastKey) scanner.startAt({ id: lastKey });
-    return scanner.exec().then((res) => res.slice(0, limit));
+    const pkgExists = await this.findOne(packageId, ['id']);
+    if (!pkgExists) throw new NotFoundException();
+    const prjQuery = this.prj
+      .query()
+      .using('PkgGSI')
+      .where('type')
+      .eq(packageId)
+      .attributes(['id'])
+      .limit(limit);
+    if (lastKey) prjQuery.startAt({ type: packageId, id: lastKey });
+    if (sort) prjQuery.sort(sort);
+    const prjIds = await prjQuery.exec();
+    const resolvedProjects = await this.prj.batchGet(
+      prjIds.map((prj) => ({
+        id: prj.id,
+        type: prj.id,
+      }))
+    );
+    return plainToInstance(ProjectDto, resolvedProjects);
   }
 }
