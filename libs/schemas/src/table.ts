876fc37a8bbcd6f0690d1930a520abde680029ab
Razin Idzuddin <razin99@gmail.com>
2022-04-09 22:50:33 +1000

    feat(table): single table design schema

---
+++ libs/schemas/src/table.ts
@@ -0,0 +1,51 @@
+import { Schema } from 'dynamoose';
+
+export const MainSchema = new Schema({
+  pk: {
+    type: String,
+    hashKey: true,
+  },
+  sk: {
+    type: String,
+    rangeKey: true,
+    index: {
+      global: true,
+      name: 'InverseGSI',
+      rangeKey: 'pk',
+    },
+  },
+  type: {
+    type: String,
+    index: {
+      global: true,
+      name: 'TypeGSI',
+      rangeKey: 'pk',
+    },
+  },
+
+  // All entities
+  name: {
+    type: String,
+  },
+
+  // PKG & PRJ
+  url: {
+    type: String,
+  },
+
+  // PKG
+  version: {
+    type: String,
+  },
+  checksum: {
+    type: String,
+  },
+
+  // VLN
+  description: {
+    type: String,
+  },
+  severity: {
+    type: Number,
+  },
+});
