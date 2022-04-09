5649064bd8316c58ad505c77417049dcd3cd0fe6
Razin Idzuddin <razin99@gmail.com>
2022-04-10 02:06:51 +1000

    feat(schema): defined project entity and ways to transform from ddb doc to object

--- libs/schemas/src/entities/project.ts
+++ libs/schemas/src/entities/project.ts
@@ -1,20 +1,25 @@
 import { IsNonEmptyString } from '@core/validator';
-import { Expose } from 'class-transformer';
-import { IsUrl, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose, instanceToPlain, plainToInstance } from 'class-transformer';
+import { Equals, IsUrl, Matches, validateSync } from 'class-validator';
+import {
+  BaseEntity,
+  EntityConstructor,
+  EntityType,
+  KeyPrefix,
+  MainTableOverlap,
+} from './entity';
 
-export class ProjectEntity {
-  @IsNonEmptyString()
-  @Matches(/^PRJ#/)
-  @Expose()
-  @ApiProperty()
-  id: string;
+const prjKeyRegex = new RegExp(`^${KeyPrefix.Project}#`);
 
-  @IsNonEmptyString()
-  @Matches(/^PRJ#|^PKG#/)
-  @Expose()
-  @ApiProperty()
-  type: string;
+export type PlainProjectEntity = Omit<
+  MainTableOverlap<ProjectEntity>,
+  'pk' | 'sk' | 'type'
+> & { id: string };
+
+export class ProjectEntity extends BaseEntity {
+  @Equals(EntityType.Project)
+  type: EntityType.Project;
 
   @IsNonEmptyString()
   @Expose()
@@ -25,4 +30,41 @@ export class ProjectEntity {
   @Expose()
   @ApiProperty()
   url: string;
+
+  @Matches(prjKeyRegex)
+  @Expose()
+  @ApiProperty()
+  get id(): string {
+    return this.pk;
+  }
+
+  @Matches(prjKeyRegex)
+  get pk(): string {
+    return `${KeyPrefix.Project}#${this.name}`;
+  }
+
+  @Matches(prjKeyRegex)
+  get sk(): string {
+    return this.pk;
+  }
+
+  public toPlain(): PlainProjectEntity {
+    return instanceToPlain(this, {
+      excludeExtraneousValues: true,
+    }) as PlainProjectEntity;
+  }
+
+  static fromDocument(plain: EntityConstructor<ProjectEntity, 'pk' | 'sk'>) {
+    const created = plainToInstance(ProjectEntity, plain);
+    if (plain.pk && created.pk !== plain.pk) {
+      throw Error('pk supplied mismatch');
+    }
+    if (plain.sk && created.sk !== plain.sk) {
+      throw Error('sk supplied mismatch');
+    }
+    const validationErrors = validateSync(created);
+    if (validationErrors.length > 0)
+      throw new Error(`Failed to validate:\n${validationErrors}`);
+    return created;
+  }
 }
