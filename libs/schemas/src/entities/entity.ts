f824ce354e2064fb2bfc2f05f053f9d96d3c76b6
Razin Idzuddin <razin99@gmail.com>
2022-04-10 15:45:56 +1000

    refactor: base entity has a static method to help construct an entity

--- libs/schemas/src/entities/entity.ts
+++ libs/schemas/src/entities/entity.ts
@@ -1,4 +1,6 @@
-import { plainToInstance } from 'class-transformer';
+import { ClassConstructor, Expose, plainToInstance } from 'class-transformer';
+import { IsEnum, validateSync } from 'class-validator';
+import { EntityType } from './enums';
 
 export class MainTableKey {
   pk: string;
@@ -19,40 +21,40 @@ export abstract class BaseEntity {
   abstract get pk(): string;
   abstract get sk(): string;
 
-  public abstract toPlain(): unknown;
+  abstract get id(): string;
+
+  public abstract toPlain(): PlainEntity<unknown>;
+
+  @Expose({ toClassOnly: true })
+  @IsEnum(EntityType)
+  type: EntityType;
 
   public keys(): MainTableKey {
     return plainToInstance(MainTableKey, { pk: this.pk, sk: this.sk });
   }
+
+  protected static _fromDocument<T extends MainTableDoc>(
+    plain: Partial<MainTableDoc>,
+    kls: ClassConstructor<T>
+  ) {
+    const created = plainToInstance(kls, plain, {
+      excludeExtraneousValues: true,
+    });
+    if (plain.pk && created.pk !== plain.pk) {
+      throw Error('pk supplied mismatch');
+    }
+    if (plain.sk && created.sk !== plain.sk) {
+      throw Error('sk supplied mismatch');
+    }
+    const validationErrors = validateSync(created);
+    if (validationErrors.length > 0)
+      throw new Error(`Failed to validate:\n${validationErrors}`);
+    return created;
+  }
 }
 
-export enum EntityType {
-  Package = 'Package',
-  Project = 'Project',
-  Vuln = 'Vuln',
-}
-
-export enum KeyPrefix {
-  Package = 'PKG',
-  Project = 'PRJ',
-  Vuln = 'VLN',
-}
-
-export type Overlap<T, U> = { [K in keyof T & keyof U]: U[K] };
-export type Optional<T extends object, K extends keyof T = keyof T> = Omit<
-  T,
-  K
-> &
-  Partial<Pick<T, K>>;
-
-/** Makes EntityConstructor easier to read */
-export type MainTableOverlap<T> = Overlap<MainTableDoc, T>;
-
-/**
- * Select properties needed to construct class T
- * Make fields in U optional
- */
-export type EntityConstructor<
-  T,
-  U extends keyof MainTableOverlap<T> = keyof MainTableOverlap<T>
-> = Optional<MainTableOverlap<T>, U>;
+type Overlap<T, U> = { [K in keyof T & keyof U]: U[K] };
+type MainTableOverlap<T> = Overlap<MainTableDoc, T>;
+export type PlainEntity<T> = Omit<MainTableOverlap<T>, 'pk' | 'sk' | 'type'> & {
+  id: string;
+};
