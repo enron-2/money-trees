a20811b53a91b73631e3b33e576aa515a07fbf97
Razin Idzuddin <razin99@gmail.com>
2022-04-10 16:57:08 +1000

    feat(schema): add vuln entity and test

--- libs/schemas/src/entities/vuln.ts
+++ libs/schemas/src/entities/vuln.ts
@@ -1,38 +1,60 @@
 import { IsNonEmptyString } from '@core/validator';
-import { Expose } from 'class-transformer';
-import { IsInt, Matches, Max, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose, instanceToPlain, Transform } from 'class-transformer';
+import { Equals, IsInt, IsOptional, Matches, Max, Min } from 'class-validator';
+import { BaseEntity, MainTableDoc, PlainEntity } from './entity';
+import { EntityType, KeyPrefix } from './enums';
 
-export class VulnEntity {
-  @IsNonEmptyString()
-  @Matches(/^PKG#/)
-  @Expose()
-  @ApiProperty()
-  id: string;
+const vulnKeyRegex = new RegExp(`^${KeyPrefix.Vuln}#`);
 
-  @IsNonEmptyString()
-  @Matches(/^VLN#/)
-  @Expose()
-  @ApiProperty()
-  type: string;
+export class VulnEntity extends BaseEntity {
+  @Equals(EntityType.Vuln)
+  type: EntityType.Vuln;
 
-  /**
-   * CVE goes here
-   */
   @IsNonEmptyString()
   @Expose()
   @ApiProperty()
   name: string;
 
   @IsNonEmptyString()
+  @IsOptional()
   @Expose()
   @ApiProperty()
-  description: string;
+  description?: string;
 
+  @Transform(({ value }) => Number(value))
   @IsInt()
   @Min(1)
   @Max(10)
   @Expose()
   @ApiProperty()
   severity: number;
+
+  @Matches(vulnKeyRegex)
+  @Expose()
+  @ApiProperty()
+  get id(): string {
+    return this.pk;
+  }
+
+  @Matches(vulnKeyRegex)
+  get pk(): string {
+    const severity = String.fromCodePoint(this.severity);
+    return `${KeyPrefix.Vuln}#${severity}#${this.name}`;
+  }
+
+  @Matches(vulnKeyRegex)
+  get sk(): string {
+    return this.pk;
+  }
+
+  public toPlain() {
+    return instanceToPlain(this, {
+      excludeExtraneousValues: true,
+    }) as PlainEntity<VulnEntity>;
+  }
+
+  static fromDocument(plain: Partial<MainTableDoc>) {
+    return BaseEntity._fromDocument(plain, VulnEntity);
+  }
 }
