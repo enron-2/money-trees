85cc3824dc90aa071ec0f2a498eeda6d06b395f3
Razin Idzuddin <razin99@gmail.com>
2022-04-11 20:19:20 +1000

    feat(http): add worstVuln to DTO objects

--- apps/http/src/app/dto.ts
+++ apps/http/src/app/dto.ts
@@ -1,15 +1,16 @@
-import { IsNonEmptyString } from '@core/validator';
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { OmitType } from '@nestjs/swagger';
 import { PackageEntity, ProjectEntity, VulnEntity } from '@schemas/entities';
 import { Expose, Transform, Type } from 'class-transformer';
-import { IsNumber, IsOptional, Matches, Max, Min } from 'class-validator';
+import { IsInt, IsOptional, Matches, Max, Min } from 'class-validator';
+
+const omitOptions = ['sk', 'pk', 'type', 'keys', 'toPlain'] as const;
 
 export class PaginationDto {
   @ApiPropertyOptional({ minimum: 1, maximum: 100, default: 10 })
   @IsOptional()
-  @Transform((param) => +param.value)
-  @IsNumber()
+  @Transform((param) => Number(param.value))
+  @IsInt()
   @Min(1)
   @Max(100)
   limit?: number;
@@ -20,14 +21,25 @@ export class PaginationDto {
   lastKey?: string;
 }
 
-export class VulnDto extends OmitType(VulnEntity, ['id', 'type']) {
-  @IsNonEmptyString()
-  @Matches(/^VLN#/)
+export class WorstSeverityDto {
   @Expose()
+  @ApiProperty()
   id: string;
+
+  @Expose()
+  @Transform((param) => Number(param.value))
+  @ApiProperty()
+  severity: number;
 }
 
-export class PackageDto extends OmitType(PackageEntity, ['type']) {}
+export class VulnDto extends OmitType(VulnEntity, omitOptions) {}
+
+export class PackageDto extends OmitType(PackageEntity, omitOptions) {
+  @Expose()
+  @ApiProperty({ type: WorstSeverityDto })
+  @Type(() => WorstSeverityDto)
+  worstSeverity?: WorstSeverityDto;
+}
 
 export class PackageDetailDto extends PackageDto {
   @Expose()
@@ -36,11 +48,16 @@ export class PackageDetailDto extends PackageDto {
   vulns?: Array<VulnDto>;
 }
 
-export class ProjectDto extends OmitType(ProjectEntity, ['type']) {}
+export class ProjectDto extends OmitType(ProjectEntity, omitOptions) {
+  @Expose()
+  @ApiPropertyOptional({ type: WorstSeverityDto })
+  @Type(() => WorstSeverityDto)
+  worstSeverity?: WorstSeverityDto;
+}
 
 export class ProjectDetailDto extends ProjectDto {
   @Expose()
   @ApiPropertyOptional({ type: [PackageDto] })
   @Type(() => PackageDto)
-  packages: Array<PackageDto>;
+  packages?: Array<PackageDto>;
 }
