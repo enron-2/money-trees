be99067fd2c2adeff561b26d0b3c36463c986ef8
Razin Idzuddin <razin99@gmail.com>
2022-04-15 23:55:47 +1000

    feat(deployment): expose function name of parser stack as lambdaName

--- apps/deployment/src/stacks/parser-stack.ts
+++ apps/deployment/src/stacks/parser-stack.ts
@@ -1,7 +1,5 @@
 import { Stack, StackProps, Construct, Duration } from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as s3n from '@aws-cdk/aws-s3-notifications';
 import { DatabaseStack } from './database-stack';
 import { join } from 'path';
 import { NodeLambdaFunc } from '../constructs';
@@ -12,6 +10,8 @@ interface ParserStackProps extends StackProps {
 }
 
 export class ParserStack extends Stack {
+  lambdaName: string;
+
   constructor(scope: Construct, id: string, props: ParserStackProps) {
     const { database, ...stackProps } = props;
     super(scope, id, stackProps);
@@ -36,22 +36,12 @@ export class ParserStack extends Stack {
       timeout: Duration.seconds(30),
     }).LambdaFunction;
 
+    this.lambdaName = parserLambda.functionName;
+
     database.grantRead(parserLambda);
     database.grantWrite(parserLambda);
 
     // TODO: if stageName == prod
     // Add 'cognito' or some other auth method
-
-    // BUG: ??? Something here creates a python function that handles notification, why can't attach directly????
-
-    const bucket = new s3.Bucket(this, `LockFileBucket`, {
-      // TODO: handle access control to allow uploads from certain sources
-      bucketName: `lock-file-bucket-${props.stageName.toLowerCase()}`,
-    });
-    bucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3n.LambdaDestination(parserLambda)
-    );
-    bucket.grantRead(parserLambda);
   }
 }
