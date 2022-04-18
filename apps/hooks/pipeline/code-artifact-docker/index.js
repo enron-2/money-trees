7eef61caaa6aead2d1fb119caed19235c906a7f6
steven <s.phung808@gmail.com>
2022-04-18 11:55:15 +1000

    perf(hooks): typescript added

--- apps/hooks/pipeline/code-artifact-docker/index.js
+++ apps/hooks/pipeline/code-artifact-docker/index.js
@@ -1,57 +1,210 @@
+'use strict';
 // handler to upload package to codeartifact
-
-const cp = require('child_process');
-const aws_sdk_2 = require('aws-sdk');
-const secretsManager = new aws_sdk_2.SecretsManager();
-
-exports.handler = async (event) => {
-  /* pull data from secrets manager */
-  const { SecretString: AWS_ACCESS_KEY_ID } = await secretsManager
-    .getSecretValue({ SecretId: 'AWS_ACCESS_KEY_ID' })
-    .promise();
-
-  cp.exec(`export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`);
-
-  const { SecretString: AWS_SECRET_ACCESS_KEY } = await secretsManager
-    .getSecretValue({ SecretId: 'AWS_SECRET_ACCESS_KEY' })
-    .promise();
-  cp.exec(`export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`);
-
-  const AWS_DEFAULT_REGION = 'ap-souteast2';
-  cp.exec(`export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}`);
-
-  /* login to codeartifact */
-  const caCmd = `codeartifact login --tool npm --domain ${event.codeArtifactDomain} --repository ${event.codeArtifactRepo} --namespace ${event.codeArtifactNamespace}`;
-  let aws = cp.spawnSync('aws', caCmd.split(' '));
-  aws = {
-    stdout: aws.stdout.toString(),
-    stderr: aws.stderr.toString(),
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
   };
-
-  /* ensure entry to empty directory */
-  const entryToDownloadLocation = `mkdir -p ${event.downloadLocation} && cd ${event.downloadLocation} && rm -rf *`;
-  let entryLocation = cp.spawnSync(entryToDownloadLocation.split(' '));
-  entryLocation = {
-    stdout: entryLocation.stdout.toString(),
-    stderr: entryLocation.stderr.toString(),
+var __generator =
+  (this && this.__generator) ||
+  function (thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function () {
+          if (t[0] & 1) throw t[1];
+          return t[1];
+        },
+        trys: [],
+        ops: [],
+      },
+      f,
+      y,
+      t,
+      g;
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === 'function' &&
+        (g[Symbol.iterator] = function () {
+          return this;
+        }),
+      g
+    );
+    function verb(n) {
+      return function (v) {
+        return step([n, v]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError('Generator is already executing.');
+      while (_)
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t =
+                op[0] & 2
+                  ? y['return']
+                  : op[0]
+                  ? y['throw'] || ((t = y['return']) && t.call(y), 0)
+                  : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op;
+              break;
+            case 4:
+              _.label++;
+              return { value: op[1], done: false };
+            case 5:
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
+            case 7:
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
+            default:
+              if (
+                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
+                (op[0] === 6 || op[0] === 2)
+              ) {
+                _ = 0;
+                continue;
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1];
+                break;
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1];
+                t = op;
+                break;
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2];
+                _.ops.push(op);
+                break;
+              }
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
+          }
+          op = body.call(thisArg, _);
+        } catch (e) {
+          op = [6, e];
+          y = 0;
+        } finally {
+          f = t = 0;
+        }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
+    }
   };
-
-  /* git activity to get package */
-  const gitDownload = `wget --header='Authorization: token ${event.gitToken}' https://api.github.com/repos/${event.gitOwner}/${event.gitRepoName}/tarball/main' && mkdir ${event.gitRepoName} && tar xzf main -C ${event.gitRepoName} --strip-components 1 && cd ${event.gitRepoName}`;
-  let entryGitDownload = cp.spawnSync(gitDownload.split(' '));
-  entryGitDownload = {
-    stdout: entryGitDownload.stdout.toString(),
-    stderr: entryGitDownload.stderr.toString(),
-  };
-
-  /* publish package */
-  let npm = cp.spawnSync('npm', ['publish']);
-  npm = {
-    stdout: npm.stdout.toString(),
-    stderr: npm.stderr.toString(),
-  };
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ aws, entryLocation, entryGitDownload, npm }),
-  };
+exports.__esModule = true;
+var cp = require('child_process');
+var aws_sdk_2 = require('aws-sdk');
+var secretsManager = new aws_sdk_2.SecretsManager();
+exports.handler = function (event) {
+  return __awaiter(void 0, void 0, void 0, function () {
+    var AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY,
+      AWS_DEFAULT_REGION,
+      caCmd,
+      aws,
+      entryToDownloadLocation,
+      entryLocation,
+      gitDownload,
+      entryGitDownload,
+      npm;
+    return __generator(this, function (_a) {
+      switch (_a.label) {
+        case 0:
+          return [
+            4 /*yield*/,
+            secretsManager
+              .getSecretValue({ SecretId: 'AWS_ACCESS_KEY_ID' })
+              .promise(),
+          ];
+        case 1:
+          AWS_ACCESS_KEY_ID = _a.sent().SecretString;
+          cp.exec('export AWS_ACCESS_KEY_ID='.concat(AWS_ACCESS_KEY_ID));
+          return [
+            4 /*yield*/,
+            secretsManager
+              .getSecretValue({ SecretId: 'AWS_SECRET_ACCESS_KEY' })
+              .promise(),
+          ];
+        case 2:
+          AWS_SECRET_ACCESS_KEY = _a.sent().SecretString;
+          cp.exec(
+            'export AWS_SECRET_ACCESS_KEY='.concat(AWS_SECRET_ACCESS_KEY)
+          );
+          AWS_DEFAULT_REGION = 'ap-souteast2';
+          cp.exec('export AWS_DEFAULT_REGION='.concat(AWS_DEFAULT_REGION));
+          caCmd = 'aws codeartifact login --tool npm --domain '
+            .concat(event.codeArtifactDomain, ' --repository ')
+            .concat(event.codeArtifactRepo, ' --namespace ')
+            .concat(event.codeArtifactNamespace);
+          aws = cp.spawnSync(caCmd);
+          entryToDownloadLocation = 'mkdir -p '
+            .concat(event.downloadLocation, ' && cd ')
+            .concat(event.downloadLocation, ' && rm -rf *');
+          entryLocation = cp.spawnSync(entryToDownloadLocation);
+          gitDownload = "wget --header='Authorization: token "
+            .concat(event.gitToken, "' https://api.github.com/repos/")
+            .concat(event.gitOwner, '/')
+            .concat(event.gitRepoName, "/tarball/main' && mkdir ")
+            .concat(event.gitRepoName, ' && tar xzf main -C ')
+            .concat(event.gitRepoName, ' --strip-components 1 && cd ')
+            .concat(event.gitRepoName);
+          entryGitDownload = cp.spawnSync(gitDownload);
+          npm = cp.spawnSync('npm', ['publish']);
+          return [
+            2 /*return*/,
+            {
+              statusCode: 200,
+              body: JSON.stringify({
+                aws: aws,
+                entryLocation: entryLocation,
+                entryGitDownload: entryGitDownload,
+                npm: npm,
+              }),
+            },
+          ];
+      }
+    });
+  });
 };
---
+++ apps/hooks/pipeline/code-artifact-docker/index.ts
@@ -0,0 +1,47 @@
+// handler to upload package to codeartifact
+
+import cp = require('child_process');
+import aws_sdk_2 = require('aws-sdk');
+const secretsManager = new aws_sdk_2.SecretsManager();
+
+exports.handler = async (
+  event
+): Promise<{
+  statusCode: number;
+  body: string;
+}> => {
+  /* pull data from secrets manager */
+  const { SecretString: AWS_ACCESS_KEY_ID } = await secretsManager
+    .getSecretValue({ SecretId: 'AWS_ACCESS_KEY_ID' })
+    .promise();
+
+  cp.exec(`export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`);
+
+  const { SecretString: AWS_SECRET_ACCESS_KEY } = await secretsManager
+    .getSecretValue({ SecretId: 'AWS_SECRET_ACCESS_KEY' })
+    .promise();
+  cp.exec(`export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`);
+
+  const AWS_DEFAULT_REGION = 'ap-souteast2';
+  cp.exec(`export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}`);
+
+  /* login to codeartifact */
+  const caCmd = `aws codeartifact login --tool npm --domain ${event.codeArtifactDomain} --repository ${event.codeArtifactRepo} --namespace ${event.codeArtifactNamespace}`;
+  const aws = cp.spawnSync(caCmd);
+
+  /* ensure entry to empty directory */
+  const entryToDownloadLocation = `mkdir -p ${event.downloadLocation} && cd ${event.downloadLocation} && rm -rf *`;
+  const entryLocation = cp.spawnSync(entryToDownloadLocation);
+
+  /* git activity to get package */
+  const gitDownload = `wget --header='Authorization: token ${event.gitToken}' https://api.github.com/repos/${event.gitOwner}/${event.gitRepoName}/tarball/main' && mkdir ${event.gitRepoName} && tar xzf main -C ${event.gitRepoName} --strip-components 1 && cd ${event.gitRepoName}`;
+  const entryGitDownload = cp.spawnSync(gitDownload);
+
+  /* publish package */
+  const npm = cp.spawnSync('npm', ['publish']);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ aws, entryLocation, entryGitDownload, npm }),
+  };
+};
